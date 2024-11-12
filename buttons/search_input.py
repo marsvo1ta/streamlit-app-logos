c45e0a32a264b2df0d82f90d6bdf0adb2e4364ce
marsvo1ta <euromobilco@gmail.com>
2024-11-12 17:23:57 +0200

    search top

--- buttons/search_input.py
+++ buttons/search_input.py
@@ -12,4 +12,9 @@ def mw_search_by_iew_input(iew: str):
 
 def top_search_by_iew_input(iew: str):
     response = logos.search_top_by_iew(iew, SEARCH_MW_BODY)
+    if response.status_code not in (200, 201):
+        return {"url": response.request.url,
+                "request_body": response.request.body,
+                "status_code": response.status_code,
+                "response_body": response.json()}
     return response.json()
